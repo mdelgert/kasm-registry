ca6cbaebaf221efc8012070ac5631163ce5251f9
Matthew David Elgert <mdelgert@yahoo.com>
2026-03-03 14:54:51 -0700

    Update icon URL and revert basePath to version 1.0 in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,12 +6,12 @@ const nextConfig = {
   env: {
     name: 'Elgert',
     description: 'My Kasm supported workspaces.',
-    icon: '/img/logo.svg',
+    icon: 'https://mdelgert.github.io/kasm-registry/favicon.ico',
     listUrl: 'https://mdelgert.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.1',
+  basePath: '/kasm-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
