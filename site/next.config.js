75b9c3478f1a5301d0088f595665aed9c5becc61
Matthew David Elgert <mdelgert@yahoo.com>
2026-03-03 15:29:39 -0700

    Add Ubuntu MATE workspace configuration
    - Created a new workspace configuration file for Ubuntu MATE.
    - Defined properties such as friendly name, description, resource allocation, and run configuration.
    - Included support for amd64 and arm64 architectures.

--- site/next.config.js
+++ site/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
   distDir: '../public',
   env: {
     name: 'Elgert 1.1',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
+    description: 'My Kasm supported workspaces.',
+    icon: 'https://raw.githubusercontent.com/homarr-labs/dashboard-icons/60c85d20781a0a22f26496091d1ed8f1c1673e99/svg/kasm.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
   },
