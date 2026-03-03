cf2361d273dba2fc9d860fbd645bc8478c15d05c
Matthew David Elgert <mdelgert@yahoo.com>
2026-03-03 14:44:35 -0700

    Update environment variables and base path in next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Elgert',
+    description: 'My Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://mdelgert.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
